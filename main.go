a4fadf5940df056e43256396bb3769b2f34063df
James O'Toole <james.otoole@industrieit.com>
2015-05-28 16:48:03 +1000

    I AM A PLATFORMER NOW

--- main.go
+++ main.go
@@ -71,6 +71,10 @@ func draw() {
 	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
 	gl.LoadIdentity()
 
+	//Transform screen.
+	gl.PushMatrix()
+	gl.Translatef((1280/2)-float32((player.Body.Position().X)), 0, 0.0)
+
 	gl.Begin(gl.LINES)
 	gl.Color3f(.2, .5, .2)
 	for i := range staticLines {
@@ -102,6 +106,8 @@ func draw() {
 	gl.Translatef(float32(pos.X), float32(pos.Y), 0.0)
 	gl.Rotatef(float32(rot), 0, 0, 1)
 	drawCircle(float64(ballRadius), 60)
+	gl.PopMatrix()
+
 	gl.PopMatrix()
 }
 
